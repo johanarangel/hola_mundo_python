3d8cf4cfac26d9937cbd3d9567544aeace272ecf
hfpaglione <87154263+hfpaglione@users.noreply.github.com>
2021-07-17 21:01:33 -0300

    Update hola_mundo.py
    Guardar cambios localmente (commit)

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,17 +1,1 @@
-#!/usr/bin/env python
-'''
-Hola Mundo! [Python]
----------------------------
-Autor: Inove Coding School
-Version: 1.3
-
-Descripcion:
-Programa creado para ensayar el correcto funcionamiento
-del entorno de instalación Python
-'''
-
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
-__version__ = "1.3"
-
-print("Hola redondo y blanco Mundo!")
+print("Hola Mundo! mi nombre es Hector")

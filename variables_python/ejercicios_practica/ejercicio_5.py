086857516a1d27ef7c8732b307aacbfe1bb4166a
johanarangel <68509096+johanarangel@users.noreply.github.com>
2021-07-19 17:30:21 -0300

    Update ejercicio_5.py

--- variables_python/ejercicios_practica/ejercicio_5.py
+++ variables_python/ejercicios_practica/ejercicio_5.py
@@ -25,3 +25,9 @@ palab1_3lts = palabra_1[0]+palabra_1[1]+palabra_1[2]
 palab2_2lts = palabra_2[0]+palabra_2[1]
 suma = palab1_3lts + palab2_2lts
 print(suma)
+
+#------------También puedes acceder a los carácteres de la siguiente manera------
+palab1_3lts = palabra_1[:3]
+palab2_2lts = palabra_2[:2]
+suma = palab1_3lts + palab2_2lts
+print(suma)
